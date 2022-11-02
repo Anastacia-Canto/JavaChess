b94a38af650d5b06e1af3ec620ebf5c718f01506
Anastacia <anaanestesia@gmail.com>
2022-11-02 14:39:28 +0000

    check logic

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -36,7 +36,7 @@ public class ChessPosition {
 	
 	//converts numbered positions to positions as they are on chess
 	protected static ChessPosition fromPosition(Position position) {
-		return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+		return new ChessPosition((char)('a' + position.getColumn()), 8 - position.getRow());
 	}
 	
 	@Override
